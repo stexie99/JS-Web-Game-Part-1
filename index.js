99e367845c723ca24ceb3fee9c366328ca2ec677
Steven Xie <xies1999@hotmail.com>
2023-05-16 13:27:10 -0400

    finished adding images

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ function newImage(source,left,bottom){
     document.body.append(img)
 }
 
-
-
-
-
-
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
 
 // let pineTree = document.createElement('img')
 // pineTree.src= 'assets/pine-tree.png'
@@ -27,4 +22,28 @@ document.body.append(greenCharacter)
 // document.body.append(pineTree)
 
 newImage('assets/pine-tree.png', 450, 200)
-newImage('')
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+function newItem(){
+    let sword = document.createElement('img')
+    sword.src = 'assets/sword.png'
+    sword.position = 'fixed'
+    sword.left = '500px'
+    sword.bottom = '405px'
+    document.body.append(sword)
+}
